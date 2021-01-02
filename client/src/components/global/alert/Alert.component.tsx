173f43f6ace6bde1a0018cf3568309c50707b9df
ale917k <alessiopetrin9@gmail.com>
2021-01-02 16:04:21 +0100

    Typed Alert.component.tsx

--- client/src/components/global/alert/Alert.component.tsx
+++ client/src/components/global/alert/Alert.component.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
-
 import "./Alert.styles.scss";
+
+interface IProps {
+  severity: string;
+  message: string;
+}
 
 /**
  * Alert message component.
  * @param {string} severity - Severity of alert message (info | success | warning | error).
  * @param {string} message - Alert message to display.
- * @return {JSX} - Alert message with appropriate styles.
+ * @return {ReactNode} - Alert message with appropriate styles.
  */
-const Alert = ({ severity, message }) => {
+const Alert = ({ severity, message }: IProps): React.ReactNode => {
   return <div className={`alert ${severity}`}>{message}</div>;
-};
-
-Alert.propTypes = {
-  severity: PropTypes.string,
-  message: PropTypes.string,
 };
 
 export default Alert;
