3bc225152f4f55fcf666ffba17410972fa79c26b
ale917k <alessiopetrin9@gmail.com>
2021-01-03 15:43:14 +0100

    Typed Spinner.component.tsx

--- client/src/components/global/spinner/Spinner.component.tsx
+++ client/src/components/global/spinner/Spinner.component.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-
 import "./Spinner.styles.scss";
 
 /**
  * Spinner component, used when lazy loading pages / components.
- * @return {JSX} - Spinner animated through css keyframes.
+ * @return - Spinner animated through css keyframes.
  */
-const Spinner = () => {
+const Spinner: React.FC = () => {
   return (
     <div className="spinner-background">
       <div className="spinner"></div>
