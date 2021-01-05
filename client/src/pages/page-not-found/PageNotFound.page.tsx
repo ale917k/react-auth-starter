c26d8b8b4b9263d5031c5f1e0ce7acb621affe6e
ale917k <alessiopetrin9@gmail.com>
2021-01-05 12:07:34 +0100

    Typed PageNotFound.page.tsx

--- client/src/pages/page-not-found/PageNotFound.page.tsx
+++ client/src/pages/page-not-found/PageNotFound.page.tsx
@@ -4,8 +4,10 @@ import "./PageNotFound.styles.scss";
 
 /**
  * Basic fallback page when no page is found on selected route.
- * @return {JSX} - PageNotFound message.
+ * @return - PageNotFound message.
  */
-export default function PageNotFound() {
+const PageNotFound: React.FC = () => {
   return <div className="page-not-found">404 | Page Not Found</div>;
-}
+};
+
+export default PageNotFound;
